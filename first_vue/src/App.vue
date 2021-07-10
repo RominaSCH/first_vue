cdf53a0173e8017659d7f9e84e74ed4fd0c1608d
Romina Schann <cuqi213@gmail.com>
2021-07-10 12:14:37 +0900

    vue set

---
+++ first_vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  안녕안녕
+</template>
+
+<script>
+// yarn 이 더 안전
+// yarn run serve 하면 열림
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
