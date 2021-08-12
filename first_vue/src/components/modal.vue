5f742096986a77e0ac660d6ef8bb720ad58bd2fd
Romina Schann <cuqi213@gmail.com>
2021-08-12 22:47:48 +0900

    vue 여러가지 input

--- first_vue/src/components/modal.vue
+++ first_vue/src/components/modal.vue
@@ -5,9 +5,17 @@
       <h4>{{ products[clicked_num].title }}</h4>
       <p>{{ products[clicked_num].content }}</p>
       <!-- <input @change="month = $event.target.value"> 은 아래와 같다-->
-      <input v-model="month">
+      <input v-model.number="month">
+      <!-- v-model.number 안하면 받는 input값이 str이기때문에 +연산을 하면 값이 더해지는게 아니라 뒤꽁무니에 따라붙는 문제가 생김. -->
+      <!-- 문자를 입력하는 사람을 막을 순 없으니 다음 강의에서 막아보는걸 배워봄 -->
+      <!-- <textarea v-model="month"></textarea> textarea, select 등등 다 v-model 가능하다. -->
+      <!-- <select v-model="month">
+        <option>3</option>
+        <option>6</option>
+        <option>12</option>
+      </select> -->
       <p>{{month}}개월 월세 {{ products[clicked_num].price * month}}만 원</p>
-      <button @click="$emit('modalClose'); month = 1;" class="closeBtn">close</button>
+      <button @click="$emit('modalClose'); month = 3;" class="closeBtn">close</button>
       <!-- props는 수정 금지임. -->
 
       <!-- 부모 데이터를 자식이 쓰고 싶을 때 쓰는 것이 props
@@ -24,7 +32,8 @@ export default {
     name:"Modal",
     data(){
       return{
-        month: 1,
+        month: 3, //문자를 입력받으려면 초기값을 문자로 해야하고, 숫자를 받으려면 초기값을 숫자로!
+        //초기값이 중요하다 이마리야
 
       }
     },
